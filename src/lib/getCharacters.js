5501c881d9b29982c1d3733f60425ebd6b5bae16
Arnold Restrepo <restrepo_arnold@outlook.com>
2022-10-05 23:27:42 -0500

    Add Characters Page

--- src/lib/getCharacters.js
+++ src/lib/getCharacters.js
@@ -1,15 +1,18 @@
-import { API_URL, API_KEY, HASH, timestamp } from './api';
+import { API_URL, API_KEY, HASH, timestamp } from "./api";
 
 const options = {
-	method: 'GET',
+  method: "GET",
 };
 
 export const getCharacters = async (limit) => {
   try {
-    const response = await fetch(`${API_URL}characters?limit=${limit}&ts=${timestamp}&apikey=${API_KEY}&hash=${HASH}`, options);
+    const response = await fetch(
+      `${API_URL}characters?limit=${limit}&ts=${timestamp}&apikey=${API_KEY}&hash=${HASH}`,
+      options
+    );
     const data = await response.json();
     return data.data;
   } catch (error) {
-    return []
+    return [];
   }
-}
+};
